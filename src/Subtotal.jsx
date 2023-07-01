33d62b5295fec0677b2e499cd080f34766ae4227
Vivek Kumar <clocktower77@protonmail.com>
2023-07-02 01:30:16 +0530

    subtotal styling

--- src/Subtotal.jsx
+++ src/Subtotal.jsx
@@ -2,11 +2,33 @@ import React from 'react'
 
 import './Subtotal.css'
 
+import CurrencyFormat from 'react-currency-format'
 
 
 function Subtotal() {
   return (
-    <div>Subtotal</div>
+    <div className="subtotal">
+    <CurrencyFormat 
+        renderText = {(value) => (
+            <>
+                <p>
+                    Subtotal <strong>$20</strong>
+                </p>
+                <small className="subtotal__gift">
+                    <input type="checkbox" /> This order contains a gift
+                </small>
+            </>
+        )}
+
+        decimalScale={2}
+        value="20"
+        displayType={"text"}
+        thousandSeparator={true}
+        
+    />
+
+    <button>Proceed to Checkout</button>
+</div>
   )
 }
 
